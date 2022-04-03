9a90a1935982fc8e017f11c8825a417de78111fa
Artur <artur-1-08@inbox.ru>
2022-04-03 13:31:53 +0300

    update cod

--- SimpleSite/bboard/urls.py
+++ SimpleSite/bboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import index, by_rubric
 
 urlpatterns = [
+    #параметризованный
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
 ]
