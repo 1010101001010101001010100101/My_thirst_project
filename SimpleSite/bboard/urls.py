1c0276f54d79c03a40bee13f2ef0936ba7bc8d46
Artur <artur-1-08@inbox.ru>
2022-04-19 16:17:12 +0300

    123

--- SimpleSite/bboard/urls.py
+++ SimpleSite/bboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, by_rubric
+from .views import *
 
 urlpatterns = [
 
--- SimpleSite/bboard/views.py
+++ SimpleSite/bboard/views.py
@@ -4,18 +4,19 @@ from django.http import HttpResponse
 
 from  .models import Bb, Categories
 
-#MAIN-HOME
+#MAIN-HOME ---- index.html
 def index(request):
    bbs = Bb.objects.all()
    rubrics = Categories.objects.all()
-   context = {'bbs' : bbs, 'rubrics' : rubrics}
+   context = {'bbs' : bbs, 'rubrics' : rubrics,}
    return render(request, 'bboard/index.html', context)
 
-#CATEGORIES
+#CATEGORIES ------ by_categories.html
 def by_rubric(request, rubric_id):
    bbs = Bb.objects.filter(rubric=rubric_id)
    rubrics = Categories.objects.all()
    current_rubric = Categories.objects.get(pk=rubric_id)
    context = {'bbs' : bbs, 'rubrics' : rubrics, 'current_rubric' : current_rubric}
    return render(request, 'bboard/by_categories.html', context)
+
 
